4eaa35174c821556f45628fe4771cb76928c2ca1
Tanmaykk <tanmaykachroo@gmail.com>
2022-04-29 22:14:01 +0530

    Homepage component changes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -10,13 +10,13 @@ function HomePage(props) {
             <PageHeader/>
             <div className={"home-page"}>
                 <div>
-                    <h1 className={"home-title"}>Code for Good</h1>
+                    <h1 className={"home-title"}>Hack DSC</h1>
                     <div className={"home-circle"}></div>
-                    <h3 className={"home-dates"}>December 5 - 6, 2020</h3>
+                    <h3 className={"home-dates"}>May 5 - 6, 2022</h3>
                 </div>
-                <a style={{textDecoration: "none" }}href="https://tinyurl.com/hd-hacker-guide" target="_blank" rel="noopener noreferrer">
-                    <Fab variant={"extended"} className={"home-button"}>Hacker Guide Here!</Fab>
-                </a>
+                {/* <a style={{textDecoration: "none" }}href="https://tinyurl.com/hd-hacker-guide" target="_blank" rel="noopener noreferrer"> */}
+                    <Fab variant={"extended"} className={"home-button"}>Register Now!</Fab>
+                {/* </a> */}
             </div>
         </div>
     )
