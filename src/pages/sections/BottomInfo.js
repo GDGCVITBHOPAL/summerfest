4c243eaa22ef496dad6d10e4da256d656e354ef6
Tanmaykk <tanmaykachroo@gmail.com>
2022-04-30 18:56:18 +0530

    Apply now URL changed

--- src/pages/sections/BottomInfo.js
+++ src/pages/sections/BottomInfo.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React from "react";
 import { Fab } from "@material-ui/core";
-import '../Sections.css';
-import './BottomInfo.css';
+import "../Sections.css";
+import "./BottomInfo.css";
 
 function BottomInfo(props) {
-    return (
-        <div className={'section'}>
-            <div className={'bottom-container'}>
-                <div id="make-impact">Make Impact</div>
-                <a style={{textDecoration: "none" }}href="https://hackduke.typeform.com/to/Dilr78Te" target="_blank" rel="noopener noreferrer">
-                    <Fab variant={"extended"} className={"home-button"}>Apply Now!</Fab>
-                </a>
-                <div id="shadow"></div>
-            </div>
-        </div>
-    )
+  return (
+    <div className={"section"}>
+      <div className={"bottom-container"}>
+        <div id="make-impact">Make Impact</div>
+        <a
+          style={{ textDecoration: "none" }}
+          href="/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Fab variant={"extended"} className={"home-button"}>
+            Apply Now!
+          </Fab>
+        </a>
+        <div id="shadow"></div>
+      </div>
+    </div>
+  );
 }
 
 export default BottomInfo;
