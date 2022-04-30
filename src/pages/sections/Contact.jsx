984353d193ad5181ab5be19d40c3996573d9d94e
Tanmaykk <tanmaykachroo@gmail.com>
2022-04-30 18:55:21 +0530

    UI tweaks

--- src/pages/sections/Contact.jsx
+++ src/pages/sections/Contact.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import Contact from "../../components/Contact"
+import React from "react";
+import Contact from "../../components/Contact";
 
 const ContactPage = () => {
   return (
-    <div className={'section'}>
-            <div className={'section-page-container'}>
-                <div className={'section-content-container'}>
-                    <div className={'section-header'}>Contact Us</div>
-                    <p className={'speaker-intro'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                    <div className={"speakers"}><Contact /></div>
-                </div>
-            </div>
+    <div className={"section"}>
+      <div className={"section-page-container"}>
+        <div
+          className={"section-content-container"}
+          // Removed margin till we get sponsors ...
+          style={{ marginTop: "0" }}
+        >
+          <div className={"section-header"}>Contact Us</div>
+          <p className={"speaker-intro"}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+          </p>
+          <div className={"speakers"}>
+            <Contact />
+          </div>
         </div>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default ContactPage
+export default ContactPage;
--- src/pages/sections/Speakers.js
+++ src/pages/sections/Speakers.js
@@ -2,18 +2,19 @@ import React from "react";
 import SpeakerSlider from "../../components/SpeakerSlider";
 
 function Speakers(props) {
-    return (
-        <div className={'section'}>
-            <div className={'section-page-container'}>
-                <div className={'section-content-container'}>
-                    <div className={'section-header'}>Sponsors</div>
-                    <p className={'speaker-intro'}>Coming soon</p>
-                    <div className={"speakers"}><SpeakerSlider/></div>
-                </div>
-            </div>
-
+  return (
+    <div className={"section"}>
+      <div className={"section-page-container"}>
+        <div className={"section-content-container"}>
+          <div className={"section-header"}>Sponsors</div>
+          {/* <p className={"speaker-intro"}>Coming soon</p> */}
+          <p style={{ fontSize: "1.3rem" }}>Coming Soon... </p>
+          {/* <div className={"speakers"}>
+            <SpeakerSlider />
+          </div> */}
         </div>
-  
+      </div>
+    </div>
   );
 }
 
