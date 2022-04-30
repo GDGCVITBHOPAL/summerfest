b1797d9e4bc88d1f250c32fb81088b7aab0c1da6
Tanmaykk <tanmaykachroo@gmail.com>
2022-04-30 17:27:50 +0530

    Timer added to Events

---
+++ src/components/eventsDataArr.js
@@ -0,0 +1,24 @@
+const eventsDataArr = [
+  {
+    title: "Networking in metaverse",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    time: "2022-05-02T00:00:00+05:30",
+  },
+  {
+    title: "Merch design",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    time: "2022-05-02T00:00:00+05:30",
+  },
+  {
+    title: "Game night",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    time: "2022-05-02T00:00:00+05:30",
+  },
+  {
+    title: "Uiux compition",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    time: "2022-05-02T00:00:00+05:30",
+  },
+];
+
+export default eventsDataArr;
