53a85b1a71bed8d1c3e945dd8acdd3a1066dccf9
abhijeet007rocks8 <abhijeet007rocks8@gmail.com>
2022-05-26 23:19:01 +0530

    add and restructure sponsors

--- src/components/SpeakerSlider.js
+++ src/components/SpeakerSlider.js
@@ -14,6 +14,9 @@ import icons8 from "../graphics/Icons8.png"
 import clerky from "../graphics/clerky.jpg"
 import arcana from "../graphics/Arcana1.png"
 import atCompany from "../graphics/AtCompany.png"
+import wize from "../graphics/wize.png"
+import huddle01 from "../graphics/Huddle01.png"
+import moreficient from "../graphics/moreficient.png"
 
 // Import Swiper styles
 import 'swiper/swiper.scss';
@@ -37,12 +40,11 @@ function SpeakerSlider() {
             //onSlideChange={changeCSS}
 
             >
-
                 <SwiperSlide>
                     <div className={"speaker-container-1"}>
-                        <a href='https://devfolio.co' style={{"cursor":"pointer"}}>
+                        <a href='https://devfolio.co' target="_blank" rel="noopener noreferrer" style={{"cursor":"pointer"}}>
                             <div className={"speaker-imagebox-main"}>
-                                    <img className={"speaker-image-1"} src={devfolio} alt="" />
+                                    <img className={"speaker-image-1"} src={devfolio} alt="" style={{"cursor":"pointer"}}/>
                             </div>
                             <div className={"speaker-textbox"}>
                                 {/* <h1 className={"speaker-name"}>Brian Li</h1> */}
@@ -53,9 +55,9 @@ function SpeakerSlider() {
                 </SwiperSlide>
                 <SwiperSlide>
                     <div className={"speaker-container-1"}>
-                        <a href='https://polygon.technology/' style={{"cursor":"pointer"}}>
+                        <a href='https://polygon.technology/' target="_blank" rel="noopener noreferrer" style={{"cursor":"pointer"}}>
                             <div className={"speaker-imagebox-main"}>
-                                <img className={"speaker-image-1"} src={polygon} alt="" />
+                                <img className={"speaker-image-1"} src={polygon} alt="" style={{"cursor":"pointer"}} />
                             </div>
                             <div className={"speaker-textbox"}>
                                 {/* <h1 className={"speaker-name"}>Brian Li</h1> */}
@@ -77,9 +79,9 @@ function SpeakerSlider() {
 
                 <SwiperSlide style={{marginRight:"-80px"}}>
                     <div className={"speaker-container-2"}>
-                        <a href='https://tezos.com' style={{"cursor":"pointer"}}>
+                        <a href='https://tezos.com' target="_blank" rel="noopener noreferrer" style={{"cursor":"pointer"}}>
                             <div className={"speaker-imagebox-main"}>
-                                <img className={"speaker-image-2"} src={tezos} alt="" />
+                                <img className={"speaker-image-2"} src={tezos} alt="" style={{"cursor":"pointer"}}/>
                             </div>
                             <div className={"speaker-textbox"}>
                                 {/* <h1 className={"speaker-name"}>Brian Li</h1> */}
@@ -89,9 +91,9 @@ function SpeakerSlider() {
                 </SwiperSlide>
                 <SwiperSlide>
                     <div className={"speaker-container-2"}>
-                        <a href='https://celo.org' style={{"cursor":"pointer"}}>
+                        <a href='https://celo.org' target="_blank" rel="noopener noreferrer" style={{"cursor":"pointer"}}>
                             <div className={"speaker-imagebox-main"}>
-                                <img className={"speaker-image-2"} src={celo} alt="" />
+                                <img className={"speaker-image-2"} src={celo} alt="" style={{"cursor":"pointer"}}/>
                             </div>
                             <div className={"speaker-textbox"}>
                                 {/* <h1 className={"speaker-name"}>Brian Li</h1> */}
@@ -101,9 +103,9 @@ function SpeakerSlider() {
                 </SwiperSlide>
                 <SwiperSlide>
                     <div className={"speaker-container-2"}>
-                        <a href='https://filecoin.io' style={{"cursor":"pointer"}}>
+                        <a href='https://filecoin.io' target="_blank" rel="noopener noreferrer" style={{"cursor":"pointer"}}>
                             <div className={"speaker-imagebox-main"}>
-                                <img className={"speaker-image-2"} src={filecoin} alt="" />
+                                <img className={"speaker-image-2"} src={filecoin} alt="" style={{"cursor":"pointer"}}/>
                             </div>
                             <div className={"speaker-textbox"}>
                                 {/* <h1 className={"speaker-name"}>Brian Li</h1> */}
@@ -113,9 +115,9 @@ function SpeakerSlider() {
                 </SwiperSlide>
                 <SwiperSlide>
                     <div className={"speaker-container-2"}>
-                        <a href='https://arcana.network/' style={{"cursor":"pointer"}}>
+                        <a href='https://arcana.network/' target="_blank" rel="noopener noreferrer" style={{"cursor":"pointer"}}>
                             <div className={"speaker-imagebox-main"}>
-                                <img id="extra-large" className={"speaker-image-2"} src={arcana} alt="" />
+                                <img id="extra-large" className={"speaker-image-2"} src={arcana} alt="" style={{"cursor":"pointer"}}/>
                             </div>
                             <div className={"speaker-textbox"}>
                                 {/* <h1 className={"speaker-name"}>Brian Li</h1> */}
@@ -134,9 +136,9 @@ function SpeakerSlider() {
             >
                 <SwiperSlide>
                     <div className={"speaker-container-2"}>
-                        <a href='https://www.interviewcake.com/' style={{"cursor":"pointer"}}>
+                        <a href='https://huddle01.com/' target="_blank" rel="noopener noreferrer" style={{"cursor":"pointer"}}>
                             <div className={"speaker-imagebox-main"}>
-                                <img className={"speaker-image-2"} src={interviewcake} alt="" />
+                                <img className={"speaker-image-2"} src={huddle01} alt="" style={{"cursor":"pointer"}}/>
                             </div>
                             <div className={"speaker-textbox"}>
                                 {/* <h1 className={"speaker-name"}>Brian Li</h1> */}
@@ -146,9 +148,9 @@ function SpeakerSlider() {
                 </SwiperSlide>
                 <SwiperSlide>
                     <div className={"speaker-container-2"}>
-                        <a href='https://www.clerky.com/' style={{"cursor":"pointer"}}>
+                        <a href='' target="_blank" rel="noopener noreferrer" style={{"cursor":"pointer"}}>
                             <div className={"speaker-imagebox-main"}>
-                                <img className={"speaker-image-2"} src={clerky} alt="" />
+                                <img className={"speaker-image-2"} src={moreficient} alt="" style={{"cursor":"pointer", width:"160px", height:"160px"}}/>
                             </div>
                             <div className={"speaker-textbox"}>
                                 {/* <h1 className={"speaker-name"}>Brian Li</h1> */}
@@ -158,9 +160,9 @@ function SpeakerSlider() {
                 </SwiperSlide>
                 <SwiperSlide>
                     <div className={"speaker-container-2"}>
-                        <a href='https://icons8.com/' style={{"cursor":"pointer"}}>
+                        <a href='https://www.clerky.com/' target="_blank" rel="noopener noreferrer" style={{"cursor":"pointer"}}>
                             <div className={"speaker-imagebox-main"}>
-                                <img className={"speaker-image-2"} src={icons8} alt="" />
+                                <img className={"speaker-image-2"} src={clerky} alt="" style={{"cursor":"pointer"}}/>
                             </div>
                             <div className={"speaker-textbox"}>
                                 {/* <h1 className={"speaker-name"}>Brian Li</h1> */}
@@ -170,9 +172,54 @@ function SpeakerSlider() {
                 </SwiperSlide>
                 <SwiperSlide>
                     <div className={"speaker-container-2"}>
-                        <a href='https://atsign.com/' style={{"cursor":"pointer"}}>
+                        <a href='https://icons8.com/' target="_blank" rel="noopener noreferrer" style={{"cursor":"pointer"}}>
                             <div className={"speaker-imagebox-main"}>
-                                <img className={"speaker-image-2 extra-large"}  src={atCompany} alt="" style={{padding:"10px 5px", background:'white'}} />
+                                <img className={"speaker-image-2"} src={icons8} alt="" style={{"cursor":"pointer"}}/>
+                            </div>
+                            <div className={"speaker-textbox"}>
+                                {/* <h1 className={"speaker-name"}>Brian Li</h1> */}
+                            </div>
+                        </a>
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+            <Swiper
+                updateOnWindowResize
+                autoplay={{ delay: 3000 }}
+                speed={1000}
+                slidesPerView={1}
+                spaceBetween={30}
+                breakpoints={{ 768: { slidesPerView: 3 }, 1024: { slidesPerView: 4 }, 1260: { slidesPerView: 5 }, 1600: { slidesPerView: 6 } }}
+            >
+                <SwiperSlide>
+                    <div className={"speaker-container-3"}>
+                        <a href='https://www.interviewcake.com/' target="_blank" rel="noopener noreferrer" style={{"cursor":"pointer"}}>
+                            <div className={"speaker-imagebox-main"}>
+                                <img className={"speaker-image-2"} src={interviewcake} alt="" style={{"cursor":"pointer"}}/>
+                            </div>
+                            <div className={"speaker-textbox"}>
+                                {/* <h1 className={"speaker-name"}>Brian Li</h1> */}
+                            </div>
+                        </a>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className={"speaker-container-3"}>
+                        <a href='https://atsign.com/' target="_blank" rel="noopener noreferrer" style={{"cursor":"pointer"}}>
+                            <div className={"speaker-imagebox-main"}>
+                                <img className={"speaker-image-2 extra-large"}  src={atCompany} alt="" style={{padding:"10px 5px", background:'white', cursor:'pointer'}} />
+                            </div>
+                            <div className={"speaker-textbox"}>
+                                {/* <h1 className={"speaker-name"}>Brian Li</h1> */}
+                            </div>
+                        </a>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className={"speaker-container-3"}>
+                        <a href='https://wize.ws' target="_blank" rel="noopener noreferrer" style={{"cursor":"pointer"}}>
+                            <div className={"speaker-imagebox-main"}>
+                                <img className={"speaker-image-2 extra-large"}  src={wize} alt="" style={{ width:"70%", cursor:"pointer"}}/>
                             </div>
                             <div className={"speaker-textbox"}>
                                 {/* <h1 className={"speaker-name"}>Brian Li</h1> */}
